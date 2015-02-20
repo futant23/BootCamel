75af2043021e7eff37ad23cb79e00a34ba9c54c7
b1050502 <b1050502@A5470394.ne.nos.boeing.com>
2015-02-20 09:13:35 -0500

    Initial

--- src/main/java/rtsw/bootcamel/Application.java
+++ src/main/java/rtsw/bootcamel/Application.java
@@ -5,12 +5,14 @@
  */
 package rtsw.bootcamel;
 
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,13 @@ public class Application extends SpringBootServletInitializer {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx =SpringApplication.run(Application.class, args);
+        
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            log.info(beanName);
+        }
     }
 
 //    @Override
